f7f0a23999984ebd724b27866862a6b257b67dea
Vpp2000 <victor.ponce.p@uni.pe>
2023-05-29 15:21:15 -0500

    Added DTOs to handle request and responses

---
+++ src/main/java/com/vpp97/demo/dto/request/CreateCurrencyExchangeRequest.java
@@ -0,0 +1,27 @@
+package com.vpp97.demo.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateCurrencyExchangeRequest {
+    @NotNull
+    @NotEmpty
+    private String currencyName;
+    @NotNull
+    @NotEmpty
+    private String currencyCode;
+    @NotNull
+    @Positive
+    private BigDecimal rate;
+}
---
+++ src/main/java/com/vpp97/demo/dto/request/UpdateCurrencyExchangeRequest.java
@@ -0,0 +1,20 @@
+package com.vpp97.demo.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateCurrencyExchangeRequest {
+    @NotNull
+    @Positive
+    private BigDecimal rate;
+}
